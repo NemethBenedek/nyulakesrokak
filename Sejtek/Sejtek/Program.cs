d5c9d3693fa955eef133b543f0fe4b4afb343605
NemethBenedek <113015566+NemethBenedek@users.noreply.github.com>
2023-10-15 18:59:29 +0200

    Add files via upload
    Kész a Grid

---
+++ Sejtek/Sejtek/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+
+class GameOfLife
+{
+    static void Main()
+    {
+        Console.Write("Add meg a rács Sorainak nagyságát (Legyen a szám 5 és 15 kötötti érték): ");
+        int rows = int.Parse(Console.ReadLine());
+        if (rows < 5)
+        {
+            Console.WriteLine("Nem megfelelő adat!");
+        }
+        else if (rows > 15)
+        {
+            Console.WriteLine("Nem megfelelő adat!");
+        }
+        Console.Write("Add meg a rács Oszlopainak nagyságát: ");
+        int cols = int.Parse(Console.ReadLine());
+
+        if (rows < 5)
+        {
+            Console.WriteLine("Nem megfelelő adat!");
+        }
+        else if (rows > 15)
+        {
+            Console.WriteLine("Nem megfelelő adat!");
+        }
+        bool[,] grid = new bool[rows, cols];
+        bool[,] newGrid = new bool[rows, cols];
+        bool[,] prevGrid = new bool[rows, cols]; 
+
+       
+
+        int generation = 0;
+        int generationsWithoutChange = 0; 
+
+      
+
+        
+    }
+
+    
+
+    static void DisplayGrid(bool[,] grid, int rows, int cols, bool[,] prevGrid)
+    {
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+                ConsoleColor backgroundColor = grid[row, col] ? ConsoleColor.Green : ConsoleColor.Red;
+
+                if (grid[row, col] == prevGrid[row, col])
+                {
+                    backgroundColor = ConsoleColor.DarkYellow; 
+                }
+
+                Console.BackgroundColor = backgroundColor;
+                Console.Write("  ");
+                Console.ResetColor();
+            }
+            Console.WriteLine();
+        }
+    }
+
+    
+
+}
